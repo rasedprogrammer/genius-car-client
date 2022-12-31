e506a0f6e69ff78d3091da5792f61d2029edc109
rasedprogrammer <rasedprogrammer@gmail.com>
2022-12-31 17:24:52 +0600

    complete project

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,13 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
 	const [services, setService] = useState([]);
+	const [isAsc, setISAsc] = useState(true);
+	const [search, setSearch] = useState("");
+	const searchRef = useRef();
+
 	useEffect(() => {
-		fetch("https://genius-car-server-eta-one.vercel.app/services")
+		fetch(
+			`https://genius-car-server-eta-one.vercel.app/services?search=${search}&order=${
+				isAsc ? "asc" : "desc"
+			}`
+		)
 			.then((res) => res.json())
 			.then((data) => setService(data));
-	}, []);
+	}, [isAsc, search]);
+
+	const handleSearch = () => {
+		setSearch(searchRef.current.value);
+	};
 	return (
 		<div>
 			<div className="text-center">
@@ -18,6 +30,19 @@ const Services = () => {
 					humour, <br /> or randomised words which don't look even slightly
 					believable.{" "}
 				</p>
+				<input
+					className="input input-bordered input-xs w-full max-w-xs mr-5"
+					ref={searchRef}
+					type="text"
+				/>{" "}
+				<button onClick={handleSearch} className="btn btn-active btn-ghost">
+					Search
+				</button>{" "}
+				<br />
+				<br />
+				<button className="btn btn-primary" onClick={() => setISAsc(!isAsc)}>
+					{isAsc ? "asc" : "desc"}
+				</button>
 			</div>
 			<div className="grid gap-6 py-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 				{services.map((service) => (
